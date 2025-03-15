d841a9de428b332c3065d6671473df3ef3821e5f
seeno <sina.1999.rahimi@gmail.com>
2025-03-15 21:19:14 +0330

    saving deepLink

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
+  "**/*.(ts|tsx)": () => "bun tsc --noEmit",
 
   "**/*.(ts|tsx|js)": (filenames) => [
-    `yarn eslint --fix --resolve-plugins-relative-to ${filenames.join(" ")}`,
+    `bun eslint --fix --resolve-plugins-relative-to ${filenames.join(" ")}`,
     `yarn prettier --write ${filenames.join(" ")}`,
   ],
 
